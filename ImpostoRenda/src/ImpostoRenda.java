7b34bed5276c58a59704fd261240201b07485f51
Natália Doreto <dorettto@icloud.com>
2025-05-15 09:53:56 -0300

    [feat] Update

--- ImpostoRenda/src/ImpostoRenda.java
+++ ImpostoRenda/src/ImpostoRenda.java
@@ -15,18 +15,48 @@ public class ImpostoRenda {
                 System.out.println("Insira seu CPF: ");
                 cpf = scan.nextLine();
                 
-                //----------------------------------Cálculo de Salário------------------------------------//
+                //--------------------------------Cálculo de Salário Anual--------------------------------//
                 Double salario = 0.0;
                 for (int i = 1; i < 13; i++){
-                        System.out.println("Insira seus rendimentos de "+i+ "/2024");
+                        System.out.println("Informe seus rendimentos de "+i+ "/2024");
                         salario += scan.nextDouble();
                 }
-                
 
+                //----------------------------------Cálculo de Imposto------------------------------------//
+                Double baseCalculo, aliquota, impostoBruto, deducao, impostoLiquido;
+                baseCalculo = salario*0.8;
+                        if (baseCalculo <= 27870.40){
+                                aliquota = 0.0;
+                                impostoBruto = baseCalculo*(aliquota/100);
+                                deducao = 0.0;
+                                impostoLiquido = impostoBruto - deducao;
+                        } else if (baseCalculo >= 27870.41 && baseCalculo <= 37751.55){
+                                aliquota = 7.5;
+                                impostoBruto = baseCalculo*(aliquota/100);
+                                deducao = 2089.83;
+                                impostoLiquido = impostoBruto - deducao;
+                        } else if (baseCalculo >= 37751.56 && baseCalculo <= 50148.25){
+                                aliquota = 15.0;
+                                impostoBruto = baseCalculo*(aliquota/100);
+                                deducao = 5268.16;
+                                impostoLiquido = impostoBruto - deducao;
+                        } else if (baseCalculo >= 50148.26 && baseCalculo <= 64259.59){
+                                aliquota = 22.5;
+                                impostoBruto = baseCalculo*(aliquota/100);
+                                deducao = 9576.05;
+                                impostoLiquido = impostoBruto - deducao;
+                        } else {
+                                aliquota = 27.5;
+                                impostoBruto = baseCalculo*(aliquota/100);
+                                deducao = 13932.41;
+                                impostoLiquido = impostoBruto - deducao;
+                        }
+                //---------------------------------------Saídas-------------------------------------------//
                 
-                
-
-
+                System.out.println("--------------------------------------------------------------------");
+                System.out.println(nome+", seu rendimento anual foi de: R$"+salario);
+                System.out.println("Sua base de cálculo para o imposto é de R$"+baseCalculo+" resultando em uma alíquota de "+aliquota+"%");
+                System.out.println("");
                 scan.close();
 	}
 
